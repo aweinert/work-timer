72c9c3828b1e0847c00ab375605b95f698244af2
Alexander Weinert <alexander.weinert@rwth-aachen.de>
2013-08-13 16:37:23 +0200

    Create interfaces in DatabaseController

--- controllers/DatabaseController.py
+++ controllers/DatabaseController.py
@@ -23,3 +23,66 @@ class DatabaseController:
 		self.project_controller = ProjectController(self._db_connection, self.contract_controller)
 		self.worktime_controller = WorktimeController(self._db_connection, self.project_controller, self.contract_controller)
 
+		# CRUD-interface for contracts
+		def create_contract(self, name, start, end, hours):
+			pass
+		
+		def retrieve_all_contracts(self):
+			pass
+		
+		def retrieve_contract_by_id(self, contract_id):
+			pass
+		
+		def update_contract(self, contract):
+			pass
+		
+		def delete_contract(self, contract):
+			pass
+
+		# CRUD-interface for projects
+		def create_project(self, name, contract):
+			pass
+		
+		def retrieve_all_projects(self):
+			pass
+		
+		def retrieve_project_by_id(self, project_id):
+			pass
+		
+		def update_project(self, project):
+			pass
+		
+		def delete_project(self, project):
+			pass
+
+		# CRUD-interface for categories
+		def create_category(self, name):
+			pass
+		
+		def retrieve_all_categories(self):
+			pass
+		
+		def retrieve_category_by_id(self, category_id):
+			pass
+		
+		def update_category(self, category):
+			pass
+		
+		def delete_category(self, category):
+			pass
+		
+		# CRUD-interface for times
+		def create_worktime(self, name):
+			pass
+		
+		def retrieve_all_worktimes(self):
+			pass
+		
+		def retrieve_worktime_by_id(self, worktime_id):
+			pass
+		
+		def update_worktime(self, worktime):
+			pass
+		
+		def delete_worktime(self, worktime):
+			pass
