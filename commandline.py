330461ff1e92e59267645db47e1f9537fcaa68a2
Alexander Weinert <alexander.weinert@rwth-aachen.de>
2013-08-20 12:13:35 +0200

    Add functionality for appending past worktimes

--- commandline.py
+++ commandline.py
@@ -26,10 +26,11 @@ def show_menu():
     configuration = {
          1: { "description": "Start Work", "function": start_work},
          2: { "description": "Stop Work", "function": stop_work},
-         3: { "description": "Enter new Contract", "function": enter_contract},
-         4: { "description": "Enter new Project", "function": enter_project},
-         5: { "description": "Enter new Category", "function": enter_category},
-         6: { "description": "Show Info", "function": show_info}
+         3: { "description": "Enter Time", "function": enter_work},
+         4: { "description": "Enter new Contract", "function": enter_contract},
+         5: { "description": "Enter new Project", "function": enter_project},
+         6: { "description": "Enter new Category", "function": enter_category},
+         7: { "description": "Show Info", "function": show_info}
        }
     default_choice = 1
     
@@ -60,6 +61,27 @@ def stop_work(controller):
     for time in running_worktimes:
         time.end = datetime.datetime.now()
         controller.update_worktime(time)
+        
+def enter_work(controller):
+    project = choose_domain_object("Please choose a project", controller.retrieve_all_projects)
+    category = choose_domain_object("Please choose a category", controller.retrieve_all_categories)
+    description = raw_input("Please enter a short description: ")
+    
+    start_date = dateutil.parser.parse(raw_input("Please enter the start date of the work: ")).date()
+    start_time = dateutil.parser.parse(raw_input("Please enter the start time of the work: ")).time()
+    start = datetime.datetime(start_date.year, start_date.month, start_date.day, start_time.hour, start_time.minute, start_time.second, start_time.microsecond)
+    
+    end_date_string = raw_input("Please enter the end date of the work (" + str(start_date) + "): ")
+    if end_date_string == "":
+        end_date = start_date
+    else:
+        end_date = dateutil.parser.parse(end_date_string).date()
+
+    end_time = dateutil.parser.parse(raw_input("Please enter the end time of the work: ")).time()
+
+    end = datetime.datetime(end_date.year, end_date.month, end_date.day, end_time.hour, end_time.minute, end_time.second, end_time.microsecond)
+    
+    controller.create_worktime(project, category, start, end, description)
 
 def enter_contract(controller):
     name = raw_input("Please enter name of new contract: ")
